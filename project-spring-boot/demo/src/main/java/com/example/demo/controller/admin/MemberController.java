deb90f18ad7f7da15a0c6dfc3ae683a83afb0df6
ganaab0000 <ganaab0000@gmail.com>
2021-04-02 15:59:02 +0900

    admin member - prototype

--- project-spring-boot/demo/src/main/java/com/example/demo/controller/admin/MemberController.java
+++ project-spring-boot/demo/src/main/java/com/example/demo/controller/admin/MemberController.java
@@ -1,5 +1,19 @@
 package com.example.demo.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
 public class MemberController {
+
+	// 메인 페이지
+	@GetMapping("/admin/member")
+	public String index() {
+		log.info("/admin/member");
+		return "admin/member/index";
+	}
 
 }
