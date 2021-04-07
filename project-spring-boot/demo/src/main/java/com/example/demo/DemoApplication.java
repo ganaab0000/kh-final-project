c9effba28c81e671944566604cefad359ab87c6b
ganaab0000 <ganaab0000@gmail.com>
2021-04-08 05:09:46 +0900

    Filter Test

--- project-spring-boot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ project-spring-boot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
+@ServletComponentScan
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.demo.repository")
 @PropertySource({
---
+++ project-spring-boot/demo/src/main/java/com/example/demo/filter/TestFilter.java
@@ -0,0 +1,32 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//@WebFilter(urlPatterns = "*")
+@Component
+public class TestFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO 전처리
+		//System.out.println("start");
+		chain.doFilter(request, response);
+		//System.out.println("end");
+		// TODO 후처리
+
+	}
+
+}
